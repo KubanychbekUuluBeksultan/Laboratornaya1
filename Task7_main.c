2b7df6fb7143a1a198cfbe1c9c27f3e69b9fbf75
KubanychbekUuluBeksultan <50055848+KubanychbekUuluBeksultan@users.noreply.github.com>
2019-04-30 02:22:55 +0300

    Create Task7_main.c

---
+++ Task7_main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "func.h"
+#include "task6_func.c"
+
+//Option 1
+void main()
+{
+	x = 2.7;
+	func();
+	printf("x =  %.4f\nf(x) = %.4f", x, otvet);
+	printf("\nGive me x:\n");
+	scanf("%f", &x);
+	func();
+	printf("x =  %.4f\nf(x) = %.4f\n", x, otvet);
+	system("pause");
+    return;
+}
