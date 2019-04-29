505bc84b49256e03738d78374ef94662593586d1
KubanychbekUuluBeksultan <50055848+KubanychbekUuluBeksultan@users.noreply.github.com>
2019-04-30 02:06:04 +0300

    Update Task1.c

--- Task1.c
+++ Task1.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 
+//Option 1
+void main()
+{
+	float x = 2.4;
+	float fx = pow(pow(3*x+2,2)-24*x,0.5)/(3*pow(x,0.5)-2/pow(x,0.5));
+	printf("x =  %.4f\nf(x) = %.4f", x, fx);
+	printf("\nGive me x:\n");
+	scanf("%f", &x);
+	fx = pow(pow(3*x+2,2)-24*x,0.5)/(3*pow(x,0.5)-2/pow(x,0.5));
+	printf("x =  %.4f\nf(x) = %.4f\n", x, fx);
+	system("pause");
+    return;
+}
